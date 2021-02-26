45b2509d211e065d0d859531dc810a0231ab1aeb
Bibek <bibekkoirala54@gmail.com>
2021-02-26 17:41:13 +0545

    First draft

--- minor/app.js
+++ minor/app.js
@@ -11,17 +11,15 @@ const blockchainController = require('./controllers/blockchainController');
 const responseController = require('./controllers/responseController');
 const { verifySignUp } = require("./middlewares");
 const logincontroller = require("./controllers/auth.controller");
-const {enrollAdmin} = require('./controllers/blockchainController')
-const dotenv = require('dotenv');
+const { enrollAdmin } = require('./controllers/blockchainController')
 const checkAuth = require("./middlewares/check-auth")
-dotenv.config();
 const Doctor = require('./models/Doctors')
 const port = process.env.PORT || 3000;
 
 const cors = require("cors");
 var corsOptions = {
-    origin: "http://localhost:8081"
-  };
+  origin: "http://localhost:8081"
+};
 
 
 // require("./db/conn");
@@ -39,48 +37,51 @@ app.set('views', './views')
 app.set('view engine', 'ejs')
 
 //Db connection start
-mongoose.Promise =global.Promise;
-mongoose.connect('mongodb://localhost:27017/test',{useNewUrlParser:true})
-.then(()=> console.log('connection sucessful'))
-.catch((err) => console.log('connection failed'))
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.connection.on('error', (err) => {
+	console.error('Mongo failed to connect');
+});
 
 
 //enrollAdmin();  
 function initial() {
-    Role.estimatedDocumentCount((err, count) => {
-      if (!err && count === 0) {
-        new Role({
-          name: "patient"
-        }).save(err => {
-          if (err) {
-            console.log("error", err);
-          }
-  
-          console.log("added 'patient' to roles collection");
-        });
-  
-        new Role({
-          name: "doctor"
-        }).save(err => {
-          if (err) {
-            console.log("doctor", err);
-          }
-  
-          console.log("added 'doctor' to roles collection");
-        });
-  
-        new Role({
-          name: "admin"
-        }).save(err => {
-          if (err) {
-            console.log("error", err);
-          }
-  
-          console.log("added 'admin' to roles collection");
-        });
-      }
-    });
-  }
+  Role.estimatedDocumentCount((err, count) => {
+    if (!err && count === 0) {
+      new Role({
+        name: "patient"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'patient' to roles collection");
+      });
+
+      new Role({
+        name: "doctor"
+      }).save(err => {
+        if (err) {
+          console.log("doctor", err);
+        }
+
+        console.log("added 'doctor' to roles collection");
+      });
+
+      new Role({
+        name: "admin"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'admin' to roles collection");
+      });
+    }
+  });
+}
 
 // module.exports = function(app) {
 //     app.use(function(req, res, next) {
@@ -91,18 +92,18 @@ function initial() {
 //       next();
 //     });
 app.post('/signup',
-[
-  verifySignUp.checkDuplicateUsernameOrEmail,
-  verifySignUp.checkRolesExisted
-],
-// logincontroller.signup
-//blockchainController.enrollAdmin,
-blockchainController.registerAndEnrollUser,
-responseController.ca,
-); 
-    
-    
-      
+  [
+    verifySignUp.checkDuplicateUsernameOrEmail,
+    verifySignUp.checkRolesExisted
+  ],
+  // logincontroller.signup
+  //blockchainController.enrollAdmin,
+  blockchainController.registerAndEnrollUser,
+  responseController.ca,
+);
+
+
+
 
 app.post("/signin", logincontroller.signin);
 
@@ -112,11 +113,11 @@ app.post("/signin", logincontroller.signin);
 // };
 
 app.get('/', (req, res) => {
-    res.render('index')
+  res.render('index')
 })
 
 app.get('/home', (req, res) => {
-    res.render('home')
+  res.render('home')
 })
 
 app.get('/homedoctor', (req, res) => {
@@ -124,16 +125,16 @@ app.get('/homedoctor', (req, res) => {
 })
 
 app.get('/about', (req, res) => {
-    res.render('about-us')
+  res.render('about-us')
 })
 
 app.get('/tracking', (req, res) => {
-    res.render('tracking')
+  res.render('tracking')
 })
 
 app.get('/contact', (req, res) => {
-    res.render('contact-us')
-    req.session.destroy()
+  res.render('contact-us')
+  req.session.destroy()
 })
 
 app.get('/view_doctor', (req, res) => {
@@ -141,7 +142,7 @@ app.get('/view_doctor', (req, res) => {
 })
 
 app.get('/doctor_details', (req, res) => {
-    res.render('doctor_details')
+  res.render('doctor_details')
 })
 
 app.get('/quotedoctor', (req, res) => {
@@ -151,36 +152,36 @@ app.get('/quotedoctor', (req, res) => {
 app.get('/quote', (req, res) => {
   var token = req.headers.token;
   console.log(token)
-    res.render('quote',{
-        data:{},
-        // header: new Headers({
-        //   'user': token
-        })
+  res.render('quote', {
+    data: {},
+    // header: new Headers({
+    //   'user': token
+  })
 
-    })
-app.get('/doctor_entry', (req,res) => {
-  res.render('form',{
-      data:{}
+})
+app.get('/doctor_entry', (req, res) => {
+  res.render('form', {
+    data: {}
   })
 })
-app.post('/doctor_entry', function(req,res){
-  res.render('form',{
-    data:req.body
+app.post('/doctor_entry', function (req, res) {
+  res.render('form', {
+    data: req.body
   })
   console.log(req.body.doctorName)
   var doctor = new Doctor({
-          doctorName :req.body.doctorName,
-          NMCNumber : req.body.NMCNumber,
-          //hospitalName: res.body.hospitalName,
-          qualification: req.body.qualification,
-          speciality: req.body.speciality,
+    doctorName: req.body.doctorName,
+    NMCNumber: req.body.NMCNumber,
+    //hospitalName: res.body.hospitalName,
+    qualification: req.body.qualification,
+    speciality: req.body.speciality,
 
   })
-          var promise = doctor.save()
-          promise.then((doctor) => {
-          console.log("user saved",doctor)
-          })
-          
+  var promise = doctor.save()
+  promise.then((doctor) => {
+    console.log("user saved", doctor)
+  })
+
 })
 // app.get('/token', function(req, res){
 //   var token = jwt.sign({id: login.id}, config.secret ,{expiresIn: 120});
@@ -205,27 +206,28 @@ app.post('/doctor_entry', function(req,res){
 //         console.log("user saved",user)
 
 //     })
-    
+
 // })
 
-app.post('/quote',reportController.createMr,
-blockchainController.invokeChaincode,
-responseController.user
+app.post('/quote', reportController.createMr,
+  blockchainController.invokeChaincode,
+  responseController.user
 )
 
-app.post('/quotedoctor',reportController.createMr,
-blockchainController.invokeChaincode,
-responseController.user
+app.post('/quotedoctor', reportController.createMr,
+  blockchainController.invokeChaincode,
+  responseController.user
 )
 
-app.post('/search',function(req,res){
+app.post('/search', function (req, res) {
   console.log(req.body);
-  Doctor.find({'doctorName': {$regex:req.body.name}})
-  .then((data =>{
-    console.log(data[0].doctorName)
-    res.render('view_doctor', {data});
-  })
-  )}
+  Doctor.find({ 'doctorName': { $regex: req.body.name } })
+    .then((data => {
+      console.log(data[0].doctorName)
+      res.render('view_doctor', { data });
+    })
+    )
+}
 )
 //Listen to port 3000
 app.listen(port, () => console.info(`Listening on port ${port}`))
